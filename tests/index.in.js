f5a4b051ff10232a8a26d327f42411a234ec5234
Anton Drachev <anton@drachev.com>
2013-07-23 02:13:28 +0400

    test runner itself is rewritten using async/await

---
+++ tests/index.in.js
@@ -0,0 +1,145 @@
+﻿var exec = require("child_process").exec;
+var fs = require("fs");
+var path = require("path");
+var preprocess = require("../src/preprocess");
+var Vow = require("vow");
+
+// *** String tools ***
+var spaces = new Array(100).join(" ");
+function pad(str, sz) {
+    return (str.length >= sz) ? str.substr(0, sz) : (str + spaces.substr(0, sz - str.length));
+}
+// see: http://misc.flogisoft.com/bash/tip_colors_and_formatting
+var RED = 91, GREEN = 32, YELLOW = 93;
+function color(str, color) { return "\033[" + color + "m" + str + "\033[39m"; }
+// NOTE: should end with [21m, but it seems to screw up some terminals
+function bold(str) { return "\033[1m" + str + "\033[22m"; }
+
+// *** Bookkeeping ***
+var totals = { tests: 0, ok: 0, changed: 0, failed: 0 };
+var report = function(status) {
+    if (status in totals)
+        totals[status]++;
+    else
+        totals.failed++;
+    var colStat = { "ok": GREEN, "changed": YELLOW }[status] || RED;
+    console.log("[ " + color(pad(status, 7), colStat) + " ]");
+}
+
+var runTests = async(function(rootDir) {
+    var sections = fs.readdirSync(rootDir).filter(function(d) {
+        if (!/\d+-/.test(d))
+            return false;
+        if (process.argv.length > 2 && d.lastIndexOf(process.argv[2], 0) !== 0)
+            return false;
+        var stat = fs.statSync(path.join(rootDir, d));
+        return stat && stat.isDirectory();
+    });
+    sections.sort();
+    for (var i = 0; i < sections.length; i++)
+        await(runSection(rootDir, sections[i]));
+    console.log("Totals:");
+    console.log("    " + bold(totals.tests) + " tests" +
+        (", " + color(totals.ok, GREEN) + " ok") +
+        (totals.changed ? ", " + color(totals.changed, YELLOW) + " changed" : "") +
+        (totals.failed ? ", " + color(totals.failed, RED) + " failed" : "")
+    );
+});
+
+var runSection = async(function(root, dir) {
+    console.log("[ " + dir + " ]");
+    dir = path.join(root, dir);
+    var files = fs.readdirSync(dir).filter(function(f) { return /^\d+-/.test(f) && /\.js$/.test(f); });
+    files = files.filter(function(f) { return !/(\.tmp|\.out|\.exp)\.js$/.test(f); });
+    files.sort();
+    for (var i = 0; i < files.length; i++) {
+        totals.tests++;
+        process.stdout.write("    " + pad(files[i].replace(/\.js$/, ""), 25));
+        var test = await(runTest(dir, files[i]));
+        process.stdout.write("  " + pad(test.description || "", 60));
+        report(test.status || "???");
+        if (test.details)
+            test.details.forEach(function(line) { console.log("> " + line); });
+    }
+    console.log();
+});
+
+var runTest = async(function(dir, f) {
+    var fname, test;
+    try {
+        test = parseTest(fname = path.join(dir, f));
+    } catch (ex) {
+        return { status: "corrupt", details: [ ex ] };
+    }
+    try {
+        var processed = preprocess.doFile(test.src, { csFile: "../../src/compilerSupport" });
+        fs.writeFileSync(fname.replace(/\.js$/, ".tmp.js"), processed, "utf-8");
+        try {
+            var expected = fs.readFileSync(fname.replace(/\.js$/, ".exp.js"), "utf-8");
+        } catch (ex) { expected = ""; }
+    } catch (ex) {
+        test.status = "throw";
+        test.details = [ ex ];
+        return test;
+    }
+
+    var compare = function(expected, got) {
+        var isRegex = expected instanceof RegExp;
+        var m = isRegex ? expected.exec(got.trim()) : (expected === got.trim());
+        if (!m) {
+            test.status = "fail";
+            var exp = isRegex ? "/" + expected.source + "/" : JSON.stringify(expected);
+            test.details = [ "expected: " + exp, "got: " + JSON.stringify(got) ];
+        } else if (isRegex && test.params.assert && !test.params.assert(m)) {
+            test.status = "fail";
+            test.details = [ "assertion failed, m=" + JSON.stringify(m) ];
+        }
+    };
+
+    var run = await(execTest(f, dir));
+    if ("stdout" in test.params) {
+        compare(test.params.stdout, run.stdout);
+    } else if ("stderr" in test.params) {
+        compare(test.params.stderr, run.stderr);
+    } else if ("error" in test.params) {
+        var err = test.params.error;
+        if (!Array.isArray(err))
+            err = [err];
+        if (err.indexOf(run.error.code) === -1) {
+            test.status = "fail";
+            test.details = [ "expected error: " + err, "got error: " + error.code ];
+        }
+    } else {
+        test.status = "corrupt";
+        test.details = [ "nothing to verify, check your test" ];
+    }
+    if (test.status)
+        return test;
+    test.status = "changed";
+    if (expected === processed) {
+        test.status = "ok";
+        fs.unlinkSync(fname.replace(/\.js$/, ".tmp.js"));
+    }
+    return test;
+});
+
+var execTest = function(f, dir) {
+    var promise = Vow.promise();
+    var child = exec("node " + f.replace(/\.js$/, ".tmp.js"), { cwd: dir }, function(err, so, se) {
+        promise.fulfill({ error: err, stdout: so, stderr: se });
+    });
+    return promise;
+};
+
+var parseTest = function(f) {
+    var testSrc = fs.readFileSync(f, "utf-8");
+    // Strip UTF-8 BOM
+    if (testSrc.charAt(0) === "\uFEFF")
+        testSrc = testSrc.slice(1);
+    var splitAt = testSrc.indexOf("\n\n");
+    var params = testSrc.substr(0, splitAt).split(/[/]{3}\s*/g).join("").replace(/\\\n/g, "\\n");
+    var src = testSrc.substr(splitAt + 2);
+    return { params: eval('(' + params + ')'), src: src };
+}
+
+runTests(__dirname).then(null, console.error);
--- tests/index.js
+++ tests/index.js
@@ -1,154 +1,255 @@
-﻿var preprocess = require("../src/preprocess");
+var compilerSupport=require('..\\src\\compilerSupport.js');﻿var exec = require("child_process").exec;
 var fs = require("fs");
 var path = require("path");
-var exec = require("child_process").exec;
+var preprocess = require("../src/preprocess");
+var Vow = require("vow");
 
-var totals = { tests: 0, ok: 0, changed: 0, failed: 0 };
-
+// *** String tools ***
 var spaces = new Array(100).join(" ");
 function pad(str, sz) {
-    if (str.length >= sz)
-        return str.substr(0, sz);
-    return str + spaces.substr(0, sz - str.length);
+    return (str.length >= sz) ? str.substr(0, sz) : (str + spaces.substr(0, sz - str.length));
 }
 // see: http://misc.flogisoft.com/bash/tip_colors_and_formatting
 var RED = 91, GREEN = 32, YELLOW = 93;
-function color(str, color) {
-    return "\033[" + color + "m" + str + "\033[39m";
-}
-function bold(str) {
-    // NOTE: should end with [21m, but it seems to screw up some terminals
-    return "\033[1m" + str + "\033[22m";
-}
-function report(status) {
-    if (status === "ok")
-        totals.ok++;
-    else if (status === "changed")
-        totals.changed++;
+function color(str, color) { return "\033[" + color + "m" + str + "\033[39m"; }
+// NOTE: should end with [21m, but it seems to screw up some terminals
+function bold(str) { return "\033[1m" + str + "\033[22m"; }
+
+// *** Bookkeeping ***
+var totals = { tests: 0, ok: 0, changed: 0, failed: 0 };
+var report = function(status) {
+    if (status in totals)
+        totals[status]++;
     else
         totals.failed++;
-    var colStat = { "corrupt": RED, "throw": RED, "fail": RED, "ok": GREEN, "changed": YELLOW }[status];
+    var colStat = { "ok": GREEN, "changed": YELLOW }[status] || RED;
     console.log("[ " + color(pad(status, 7), colStat) + " ]");
 }
 
-function runTests(rootDir) {
-    var sections = fs.readdirSync(rootDir).filter(function(d) {
-        if (!/\d+-/.test(d))
-            return false;
-        if (process.argv.length > 2) {
-            if (d.lastIndexOf(process.argv[2], 0) !== 0)
-                return false;
+var runTests = function (rootDir) {
+    var __builder = new compilerSupport.TaskBuilder(), __state = 0, __awaiter, __continue = __builder.CONT, __ex;
+    var sections, i;
+    return __builder.run(function () {
+        switch (__state) {
+        case 0: {
+                sections = fs.readdirSync(rootDir).filter(function (d) {
+                    if (!/\d+-/.test(d))
+                        return false;
+                    if (process.argv.length > 2 && d.lastIndexOf(process.argv[2], 0) !== 0)
+                        return false;
+                    var stat = fs.statSync(path.join(rootDir, d));
+                    return stat && stat.isDirectory();
+                });
+                sections.sort();
+                i = 0;
+                __state = 1;
+                return __continue;
+            }
+        case 1: {
+                if (i < sections.length) {
+                    __state = 3;
+                    return __awaiter = runSection(rootDir, sections[i]);
+                } else {
+                    console.log("Totals:");
+                    console.log("    " + bold(totals.tests) + " tests" + (", " + color(totals.ok, GREEN) + " ok") + (totals.changed ? ", " + color(totals.changed, YELLOW) + " changed" : "") + (totals.failed ? ", " + color(totals.failed, RED) + " failed" : ""));
+                    __state = -1;
+                    __builder.ret();
+                    break;
+                }
+            }
+        case 3: {
+                i++;
+                __state = 1;
+                return __continue;
+            }
+        default:
+            throw 'Internal error: encountered wrong state';
         }
-        var dir = path.join(rootDir, d);
-        var stat = fs.statSync(dir);
-        return stat && stat.isDirectory();
     });
-    sections.sort();
-    var i = 0;
-    var runNextSection = function() {
-        if (i < sections.length)
-            runSection(rootDir, sections[i++], runNextSection);
-        else
-            reportTotals();
-    };
-    runNextSection();
 }
 
-function runSection(root, dir, callback) {
-    console.log("[ " + dir + " ]");
-    dir = path.join(root, dir);
-    var files = fs.readdirSync(dir).filter(function(f) { return /^\d+-/.test(f) && /\.js$/.test(f); });
-    files = files.filter(function(f) { return !/(\.tmp|\.out|\.exp)\.js$/.test(f); });
-    files.sort();
-    var i = 0;
-    var runNextTest = function() {
-        if (i < files.length) {
-            runTest(dir, files[i++], runNextTest);
-        } else {
-            console.log();
-            callback();
+var runSection = function (root, dir) {
+    var __builder = new compilerSupport.TaskBuilder(), __state = 0, __awaiter, __continue = __builder.CONT, __ex;
+    var $1;
+    var files, i, test;
+    return __builder.run(function () {
+        switch (__state) {
+        case 0: {
+                console.log("[ " + dir + " ]");
+                dir = path.join(root, dir);
+                files = fs.readdirSync(dir).filter(function (f) {
+                    return /^\d+-/.test(f) && /\.js$/.test(f);
+                });
+                files = files.filter(function (f) {
+                    return !/(\.tmp|\.out|\.exp)\.js$/.test(f);
+                });
+                files.sort();
+                i = 0;
+                __state = 1;
+                return __continue;
+            }
+        case 1: {
+                if (i < files.length) {
+                    totals.tests++;
+                    process.stdout.write("    " + pad(files[i].replace(/\.js$/, ""), 25));
+                    __state = 3;
+                    return __awaiter = runTest(dir, files[i]);
+                } else {
+                    console.log();
+                    __state = -1;
+                    __builder.ret();
+                    break;
+                }
+            }
+        case 3: {
+                $1 = __awaiter.valueOf();
+                test = $1;
+                process.stdout.write("  " + pad(test.description || "", 60));
+                report(test.status || "???");
+                if (test.details)
+                    test.details.forEach(function (line) {
+                        console.log("> " + line);
+                    });
+                i++;
+                __state = 1;
+                return __continue;
+            }
+        default:
+            throw 'Internal error: encountered wrong state';
         }
-    };
-    runNextTest();
+    });
 }
 
-function runTest(dir, f, callback) {
-    totals.tests++;
-    process.stdout.write("    " + pad(f.replace(/\.js$/, ""), 25));
-    var fname, test;
-    try {
-        test = parseTest(fname = path.join(dir, f));
-        var descr = test.params.description;
-        process.stdout.write("  " + pad(descr, 60));
-    } catch (ex) {
-        process.stdout.write("  " + pad("", 60));
-        report("corrupt");
-        console.log(">" + ex);
-        return callback();
-    }
-    try {
-        var processed = preprocess.doFile(test.src, { csFile: "../../src/compilerSupport" });
-        fs.writeFileSync(fname.replace(/\.js$/, ".tmp.js"), processed, "utf-8");
-        try {
-            var expected = fs.readFileSync(fname.replace(/\.js$/, ".exp.js"), "utf-8");
-        } catch (ex) { expected = ""; }
-    } catch (ex) {
-        report("throw");
-        console.log(">" + ex);
-        return callback();
-    }
-    
-    var child = exec("node " + f.replace(/\.js$/, ".tmp.js"), { cwd: dir }, function(error, stdout, stderr) {
-        var compare = function(expected, got) {
-            var isRegex = expected instanceof RegExp;
-            var m = isRegex ? expected.exec(got.trim()) : (expected === got.trim());
-            if (!m) {
-                report("fail");
-                var exp = isRegex ? "/" + expected.source + "/" : JSON.stringify(expected);
-                console.log(">expected: " + exp);
-                console.log(">got: " + JSON.stringify(got));
-                return false;
+var runTest = function (dir, f) {
+    var __builder = new compilerSupport.TaskBuilder(), __state = 0, __awaiter, __continue = __builder.CONT, __ex;
+    var $1, $2;
+    var fname, test, processed, expected, compare, run, err;
+    return __builder.run(function () {
+        switch (__state) {
+        case 0: {
+                __state = 1;
+                return __continue;
             }
-            if (isRegex && test.params.assert && !test.params.assert(m)) {
-                report("fail");
-                console.log(">assertion failed, m=" + JSON.stringify(m));
-                return false;
+        case 1: {
+                test = parseTest(fname = path.join(dir, f));
+                __state = 3;
+                return __continue;
             }
-            return true;
-        };
-
-        if ("stdout" in test.params) {
-            if (!compare(test.params.stdout, stdout))
-                return callback();
-        } else if ("stderr" in test.params) {
-            if (!compare(test.params.stderr, stderr))
-                return callback();
-        } else if ("error" in test.params) {
-            var err = test.params.error;
-            if (!Array.isArray(err))
-                err = [err];
-            if (err.indexOf(error.code) === -1) {
-                report("fail");
-                console.log(">expected error " + test.params.error);
-                console.log(">got error " + error.code);
-                return callback();
+        case 2: {
+                var ex = __ex;
+                $1 = {
+                    status: "corrupt",
+                    details: [ex]
+                };
+                __state = -1;
+                __builder.ret($1);
+                break;
             }
-        } else {
-            report("corrupt");
-            console.log(">nothing to verify, check your test");
-            return callback();
+        case 3: {
+                __state = 4;
+                return __continue;
+            }
+        case 4: {
+                processed = preprocess.doFile(test.src, { csFile: "../../src/compilerSupport" });
+                fs.writeFileSync(fname.replace(/\.js$/, ".tmp.js"), processed, "utf-8");
+                __state = 6;
+                return __continue;
+            }
+        case 5: {
+                var ex = __ex;
+                test.status = "throw";
+                test.details = [ex];
+                __state = -1;
+                __builder.ret(test);
+                break;
+            }
+        case 6: {
+                expected = fs.readFileSync(fname.replace(/\.js$/, ".exp.js"), "utf-8");
+                __state = 8;
+                return __continue;
+            }
+        case 7: {
+                var ex = __ex;
+                expected = "";
+                __state = 8;
+                return __continue;
+            }
+        case 8: {
+                __state = 9;
+                return __continue;
+            }
+        case 9: {
+                compare = function (expected, got) {
+                    var isRegex = expected instanceof RegExp;
+                    var m = isRegex ? expected.exec(got.trim()) : expected === got.trim();
+                    if (!m) {
+                        test.status = "fail";
+                        var exp = isRegex ? "/" + expected.source + "/" : JSON.stringify(expected);
+                        test.details = ["expected: " + exp, "got: " + JSON.stringify(got)];
+                    } else if (isRegex && test.params.assert && !test.params.assert(m)) {
+                        test.status = "fail";
+                        test.details = ["assertion failed, m=" + JSON.stringify(m)];
+                    }
+                };
+                __state = 10;
+                return __awaiter = execTest(f, dir);
+            }
+        case 10: {
+                $2 = __awaiter.valueOf();
+                run = $2;
+                if ("stdout" in test.params) {
+                    compare(test.params.stdout, run.stdout);
+                } else if ("stderr" in test.params) {
+                    compare(test.params.stderr, run.stderr);
+                } else if ("error" in test.params) {
+                    err = test.params.error;
+                    if (!Array.isArray(err))
+                        err = [err];
+                    if (err.indexOf(run.error.code) === -1) {
+                        test.status = "fail";
+                        test.details = ["expected error: " + err, "got error: " + error.code];
+                    }
+                } else {
+                    test.status = "corrupt";
+                    test.details = ["nothing to verify, check your test"];
+                }
+                if (test.status) {
+                    __state = -1;
+                    __builder.ret(test);
+                    break;
+                } else {
+                    test.status = "changed";
+                    if (expected === processed) {
+                        test.status = "ok";
+                        fs.unlinkSync(fname.replace(/\.js$/, ".tmp.js"));
+                    }
+                    __state = -1;
+                    __builder.ret(test);
+                    break;
+                }
+            }
+        default:
+            throw 'Internal error: encountered wrong state';
         }
-        if (expected === processed) {
-            fs.unlinkSync(fname.replace(/\.js$/, ".tmp.js"));
-            report("ok");
-        } else {
-            report("changed");
+    }, function (ex) {
+        var handler = [, 2, , , 5, , 7, 5, 5][__state];
+        if (handler !== undefined) {
+            __state = handler;
+            __ex = ex;
+            return __continue;
         }
-        return callback();
     });
+}
+
+var execTest = function(f, dir) {
+    var promise = Vow.promise();
+    var child = exec("node " + f.replace(/\.js$/, ".tmp.js"), { cwd: dir }, function(err, so, se) {
+        promise.fulfill({ error: err, stdout: so, stderr: se });
+    });
+    return promise;
 };
 
-function parseTest(f) {
+var parseTest = function(f) {
     var testSrc = fs.readFileSync(f, "utf-8");
     // Strip UTF-8 BOM
     if (testSrc.charAt(0) === "\uFEFF")
@@ -159,13 +260,4 @@ function parseTest(f) {
     return { params: eval('(' + params + ')'), src: src };
 }
 
-function reportTotals() {
-    console.log("Totals:");
-    console.log("    " + bold(totals.tests) + " tests" +
-        (", " + color(totals.ok, GREEN) + " ok") +
-        (totals.changed ? ", " + color(totals.changed, YELLOW) + " changed" : "") +
-        (totals.failed ? ", " + color(totals.failed, RED) + " failed" : "")
-    );
-}
-
-runTests(__dirname);
+runTests(__dirname).then(null, console.error);
