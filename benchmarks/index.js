763a157476a4908b9fdd993696e1ad743d77dc26
Anton Drachev <anton@drachev.com>
2013-12-25 01:18:17 +0400

    added benchmarks

---
+++ benchmarks/index.js
@@ -0,0 +1,100 @@
+﻿// NOTE: run with `node --expose-gc`
+
+var WARMUP_LIMIT_MS = 500;
+var WARMUP_RUN_LIMIT_MS = 100;
+var TEST_RUN_LIMIT_MS = 1000;
+var TEST_RUNS = 8;
+
+// test for async:
+try {
+    var x = async(function() { });
+} catch (ex) {
+    console.error("Please run this program with asjs enabled.");
+    console.error("Use `node --expose-gc asjs benchmarks`");
+    process.exit(1);
+}
+
+// test for available GC controls:
+try {
+    global.gc();
+} catch (ex) {
+    console.error("Please run this program with exposed gc control");
+    console.error("Use `node --expose-gc asjs benchmarks`");
+    process.exit(1);
+}
+
+/** calcs milliseconds elapsed between two hrtime() instances */
+var timeDiff = function(start, end) {
+    //  [seconds, nanoseconds]
+    var diff = [end[0] - start[0], end[1] - start[1]];
+    return diff[0] * 1e3 + diff[1] * 1e-6;
+};
+
+var prettify = function(num, significantPlaces) {
+    var roundingPlaces = Math.floor(Math.log(num) / Math.LN10) - significantPlaces + 1;
+    var rounding = Math.pow(10, roundingPlaces);
+    num = rounding * Math.round(num / rounding);
+    if (rounding < 1) {
+        try {
+            num = num.toFixed(-roundingPlaces);
+        } catch (ex) { }
+    }
+    return num;
+};
+
+var runTest = async(function(test) {
+    console.log("");
+    var updateStatus = function(status) {
+        console.log("\033[1A\033[K\r" + test.name + ": \t" + status);
+    };
+
+    var runsPerMs = 1;
+
+    updateStatus("warmup...");
+    var runAdjust = async(function(expectedMs) {
+        var runs = Math.ceil(runsPerMs * expectedMs);
+        var started = process.hrtime();
+        await(test.run(runs));
+        var elapsed = timeDiff(started, process.hrtime());
+        if (elapsed <= 0)
+            elapsed = 0.01;
+        return runsPerMs = runs / elapsed;
+    });
+
+    global.gc();
+    var runTill = +new Date() + WARMUP_LIMIT_MS;
+    while (+new Date() < runTill) {
+        await(runAdjust(WARMUP_RUN_LIMIT_MS));
+    }
+
+    var instances = [];
+    for (var i = 0; i < TEST_RUNS; i++) {
+        global.gc();
+        updateStatus("run " + (i + 1) + " of " + TEST_RUNS + "...");
+        instances.push(await(runAdjust(TEST_RUN_LIMIT_MS)));
+    }
+    instances.sort(function(a, b) { return a - b });
+    instances.pop();
+    instances.shift();
+    var avg = instances.reduce(function(a, b) { return a + b; }, 0) / instances.length;
+    updateStatus(prettify(avg, 3) + " runs/ms");
+    //console.log(process.memoryUsage()); // .heapUsed
+});
+
+
+var runTests = async(function() {
+    // lift some limits for tests
+    process.maxTickDepth = Infinity;
+
+    var tests = require("./tests");
+    console.log("Running " + tests.length + " tests.");
+    console.log("Values are mesured in operations per second, higher is better.");
+    for (var i = 0; i < tests.length; i++) {
+        await(runTest(tests[i]));
+    }
+});
+
+runTests().then(
+    function(data) { console.log("Done."); },
+    function(err) { console.error("Error: " + err.stack || err); }
+);
---
+++ benchmarks/tests.js
@@ -0,0 +1,190 @@
+﻿// https://github.com/cujojs/promise-perf-tests
+// http://thanpol.as/javascript/promises-a-performance-hits-you-should-be-aware-of/
+
+// https://github.com/petkaantonov/bluebird/wiki/Optimization-killers
+// https://github.com/petkaantonov/bluebird/tree/master/benchmark/madeup-parallel
+// https://github.com/petkaantonov/bluebird/tree/master/benchmark/doxbee-sequential
+
+// https://github.com/medikoo/deferred#performance
+
+var Vow = require("vow");
+var when = require("when");
+var lstat = require("fs").lstat;
+
+var lstatAsync = function(path) {
+    var result = Vow.promise();
+    lstat(path, function(err, stats) {
+        if (err)
+            result.reject(err);
+        else
+            result.fulfill(stats);
+    });
+    return result;
+};
+
+var scatteredPromises = function(n, maxDelay) {
+    var result = [];
+    n /= 2;
+    for (var i = 0; i < n; i++) {
+        result.push(Vow.delay("ok", (i / n) * maxDelay));
+        result.push(Vow.delay("ok", ((n - i) / n) * maxDelay));
+    }
+    return result;
+};
+
+var parallelLstats = function(n) {
+    var result = [];
+    for (var i = 0; i < n; i++)
+        result.push(lstatAsync(__filename));
+    return result;
+};
+
+module.exports = [
+    {
+        name: "Vow, sequential",
+        run: function(runs) {
+            var result = Vow.promise();
+
+            var i = runs;
+            var nextPromise = function() {
+                if (i-- > 0) {
+                    var promise = Vow.promise("ok");
+                    promise.then(nextPromise);
+                } else {
+                    result.fulfill("ok");
+                }
+            };
+            nextPromise();
+
+            return result;
+        }
+    },
+
+    {
+        name: "Vow, chained then()",
+        run: function(runs) {
+            var i = runs;
+            var promise = Vow.promise("ok");
+            for (; i-- > 0;)
+                promise = promise.then(function() { return Vow.promise("ok"); });
+            return promise;
+        }
+    },
+
+    {
+        name: "Asjs, await async",
+        run: async(function(runs) {
+            var i = runs;
+            for (; i-- > 0;) {
+                await(async(function() {
+                    return "ok";
+                })());
+            }
+            return "ok";
+        })
+    },
+
+    {
+        name: "Vow + Asjs     ",
+        run: async(function(runs) {
+            var i = runs;
+            for (; i-- > 0;)
+                await(Vow.promise("ok"));
+            return "ok";
+        })
+    },
+
+    {
+        name: "When, sequential",
+        run: function(runs) {
+            var result = Vow.promise();
+
+            var i = runs;
+            var nextPromise = function() {
+                if (i-- > 0) {
+                    var promise = when.resolve("ok");
+                    promise.then(nextPromise);
+                } else {
+                    result.fulfill("ok");
+                }
+            };
+            nextPromise();
+
+            return result;
+        }
+    },
+
+    {
+        name: "When + Asjs    ",
+        run: async(function(runs) {
+            var i = runs;
+            for (; i-- > 0;)
+                await(when.resolve("ok"));
+            return "ok";
+        })
+    },
+
+    {
+        name: "Vow, parallel rand",
+        run: function(runs) {
+            return Vow.all(scatteredPromises(500, 500));
+        }
+    },
+
+    {
+        name: "Asjs, parallel rand",
+        run: async(function(runs) {
+            var promises = scatteredPromises(500, 500);
+            for (var i = 0; i < promises.length; i++)
+                await(promises[i]);
+            return "ok";
+        })
+    },
+
+    {
+        name: "Vow, lstat sequence",
+        run: function(runs) {
+            var result = Vow.promise();
+
+            var i = runs;
+            var nextPromise = function() {
+                if (i-- > 0) {
+                    lstatAsync(__filename).then(nextPromise);
+                } else {
+                    result.fulfill("ok");
+                }
+            };
+            nextPromise();
+
+            return result;
+        }
+    },
+
+    {
+        name: "Asjs, lstat sequence",
+        run: async(function(runs) {
+            var i = runs;
+            for (; i-- > 0;) {
+                await(lstatAsync(__filename));
+            }
+            return "ok";
+        })
+    },
+
+    {
+        name: "Vow, lstat parallel",
+        run: function(runs) {
+            return Vow.all(parallelLstats(runs));
+        }
+    },
+
+    {
+        name: "Asjs, lstat parallel",
+        run: async(function(runs) {
+            var promises = parallelLstats(runs);
+            for (var i = 0; i < promises.length; i++)
+                await(promises[i]);
+            return "ok";
+        })
+    }
+];
